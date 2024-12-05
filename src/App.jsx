e6a97f3ccc17bf265a4678bf16522fa2c480c5ed
sabaif-s <sebaifmuhammed33@gmail.com>
2024-12-05 03:21:36 +0300

    Home Page E learning

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
+import React from 'react'
+import {useState} from 'react'
+ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  
+     <div className='w-full h-screen bg-gradient-to-t from-cyan-700 to-sky-300 flex justify-center items-center' >
+               <div className='w-3/4 h-3/4 relative bg-red-100' >
+                        <div className='w-full rounded-full h-28 bottom-0 bg-[#C1E8FF] absolute z-50' >
+                        </div>
+                        <div className='w-full rounded-full h-28 bottom-0 bg-[#7DA] absolute z-50' >
+                        </div>
+               </div>
+     </div>
+    
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { register } from "register-service-worker";
-register(`/service-worker.js`)
+ 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
