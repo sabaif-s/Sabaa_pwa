7b020e71a473d51ca405cebe5e0839cc243b5d06
sabaif-s <sebaifmuhammed33@gmail.com>
2024-12-07 04:25:29 +0300

    adding dynamic videoTutor using hooks for asset

--- src/components/VideoTutor.jsx
+++ src/components/VideoTutor.jsx
@@ -14,12 +14,14 @@ const  VideoTutor = () => {
     const [fetchedErrorVideoShow,setFetchedErrorVideoShow]=useState(false);
     const [fetchedErrorVideoShow2,setFetchedErrorVideoShow2]=useState(false);
     const [downloadPercentage, setDownloadPercentage] = useState(0);
-    const [handleDownloadFirstVideo,setHandleDownloadFirstVideo]=useState(false);
-    const [pauseFirst,setPauseFirst]=useState(false);
+    const [handleDownloadFirstVideo,setHandleDownloadFirstVideo]=useState(99999);
+    const [pauseFirst,setPauseFirst]=useState(99999);
     const [downloadInProgressFirst,setDownloadInProgressFirst]=useState(false);
     const [startDownloadFirstVideo,setStartDownloadFirstVideo]=useState(false);
-    const [fullyDownloadedFirst,setFullyDownloadedFirst]=useState(false);
+    const [fullyDownloadedFirst,setFullyDownloadedFirst]=useState("new");
     const [renderThisComponent,setRenderThisComponent]=useState(false);
+    const [downLoadLink,setDownLoadLink]=useState(false);
+    const [downloadingVideo,setDownLoadingVideo]=useState("");
     // useEffect(() => {
     //   let interval;
     //   if (downloadPercentage < 100) {
@@ -45,7 +47,7 @@ const  VideoTutor = () => {
       }
     },[videoAsset]);
     useEffect(() => {
-      async function downloadVideo(videoUrl) {
+      async function downloadVideo(videoUrl,downloadingUnique) {
         // Open or create an IndexedDB instance
         const initDBChunk=async ()=>{
           const db = await openDB('video-db', 1, {
@@ -165,7 +167,7 @@ const  VideoTutor = () => {
         
             // Wait for the transaction to complete
             await tx.done;
-            setFullyDownloadedFirst(true);
+            setFullyDownloadedFirst(downloadingUnique);
 
             console.log(`Records with ${attributeName} = ${attributeValue} deleted`);
         };
@@ -195,7 +197,7 @@ const  VideoTutor = () => {
       }
     
       if (startDownloadFirstVideo) {
-        downloadVideo(firstVideo);
+        downloadVideo(downLoadLink,downloadingVideo);
       }
     }, [startDownloadFirstVideo]);
     
@@ -224,208 +226,187 @@ const  VideoTutor = () => {
       console.log("error in fetching video");
       setFetchedErrorVideoShow2(true);
     }
-    const handleDownloadFirst= async ()=>{
-       setHandleDownloadFirstVideo(true);
-       setPauseFirst(true);
+    const handleDownloadFirst= async (event)=>{
+      console.log(event.target.getAttribute('id'));
+      const index=event.target.getAttribute('id');
+       setHandleDownloadFirstVideo(index);
+       setPauseFirst(index);
+       setDownLoadingVideo(videoAsset[index].uniqueName);
+       setDownLoadLink(videoAsset[index].src);
        setStartDownloadFirstVideo(true);
-       setClickedList(1);
+       setClickedList(index);
     }
     return (
-        <div className='w-full min-h-screen flex flex-col justify-start items-center bg-gradient-to-t from-gray-300 to-gray-400 pt-4' >
-                       <div className='w-full p-4 flex justify-center items-center bg-white text-gray-400 mb-6' >
-                        <h1 className='text-3xl font-semibold' >
-                            Videos
-                        </h1>
-                       </div>
-                       <div className='w-full relative bg-blue-300 flex justify-between items-center' >
-                        <div className='absolute z-10 inset-0 flex justify-center items-center' >
-                                 <div className='w-2 h-full bg-red-400' >
+        <>
+            {
+              renderThisComponent && (
+                <div className='w-full min-h-screen flex flex-col justify-start items-center bg-gradient-to-t from-gray-300 to-gray-400 pt-4' >
+                <div className='w-full p-4 flex justify-center items-center bg-white text-gray-400 mb-6' >
+                 <h1 className='text-3xl font-semibold' >
+                     Videos
+                 </h1>
+                </div>
+                <div className='w-full relative bg-blue-300 flex justify-between items-center' >
+                 <div className='absolute z-10 inset-0 flex justify-center items-center' >
+                          <div className='w-2 h-full bg-red-400' >
 
-                                 </div>
-                        </div>
-                        <div 
-                        onClick={()=>{
-                            setActiveButton(0);
-                        }}
-                        className={`w-1/2 ${backColorGrade} flex relative z-20 justify-center items-center h-full  p-4 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300 group`}>
-  <div className="text-white h-full text-2xl font-semibold group-hover:scale-105 transition-transform duration-300 ease-in-out">
-    Grade
-  </div>
-  </div>
-                        <div
-                        onClick={()=>{
-                            setActiveButton(1);
-                        }}
-                        className={` ${backColorYVideos} w-1/2 flex relative z-20 justify-center items-center h-full  p-4 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300 group`}>
-  <div className="text-white h-full text-2xl font-semibold group-hover:scale-105 transition-transform duration-300 ease-in-out">
-    Your Videos
-  </div>
-  </div>
-                       </div>
-                       {
-                        activeButton == 0 && (
-                          <div className='mt-4 w-full h-auto py-4 bg-white flex flex-col gap-y-4 justify-start items-center' >
-                          <div
-                         
-                          className="w-full h-12 flex justify-between items-center p-2 bg-gradient-to-r from-green-500 to-green-300 hover:shadow-lg transition-shadow duration-300">
-     
-     <div 
-      onClick={()=>{
-       if(clickedList == 1 && !downloadInProgressFirst){
-           setClickedList(0);
-         
-       }
-       else{
-           setClickedList(1);
-       }
-       
-      }}
-     className="w-1/2 h-full flex justify-center items-center overflow-x-hidden">
-       <span className="text-2xl text-white font-semibold w-full text-center overflow-x-hidden hover:text-gray-200 transition-colors duration-300">
-         name
-       </span>
-     </div>
-   
-    
-     <div className="w-1/2 h-full flex justify-center items-center">
-      
-       <div className="w-1/2 h-full flex justify-center items-center">
-         <span className="text-2xl text-blue-500 font-semibold hover:text-blue-400 transition-colors duration-300">
-           12
-           <span className="text-red-400 font-bold">:</span>
-           <span className="text-yellow-600">24</span>
-         </span>
-       </div>
-   
-       
-       <div className="w-1/2 h-full flex justify-center items-center">
-         <img
-         onClick={handleDownloadFirst}
-           src="/download.png"
-           className={` ${handleDownloadFirstVideo ? "hidden":""} ${fullyDownloadedFirst ? "hidden":""} h-full w-auto hover:scale-110 transition-transform duration-300`}
-           alt=""
-         />
-          <img
-         onClick={()=>{
-            setPauseFirst(false);
-            setHandleDownloadFirstVideo(false);
-         }}
-           src="/pause.png"
-           className={` ${pauseFirst ? "":"hidden"} ${fullyDownloadedFirst ? "hidden":""} h-full w-auto hover:scale-110 transition-transform duration-300`}
-           alt=""
-         />
-          <img
-         onClick={()=>{
-             console.log("finished clicked");
-         }}
-           src="/right.avif"
-           className={` ${fullyDownloadedFirst ? "":"hidden"} h-full w-auto hover:scale-110 transition-transform duration-300`}
-           alt=""
-         />
-       </div>
-     </div>
-   </div>
-   {
-       clickedList == 1 && (
-         <>
-           <div className={` ${handleDownloadFirstVideo ? "hidden":""} w-full p-4 bg-white h-44`} >
-                <video controls muted autoPlay onError={handleError} src={firstVideo} className={` ${fetchedErrorVideoShow ? "hidden":""} w-full h-full`} ></video>
-                <div className={` ${fetchedErrorVideoShow ? "":"hidden"} w-full h-full bg-gray-300 text-red-300 flex justify-center items-center`}>
-                             <span className='text-blue-300 font-semibold' >YOU NEED TO CONNECT YOUR DEVICE TO INTERNET</span>
-                </div>
-           </div>
-            <div className={` ${handleDownloadFirstVideo ? "":"hidden"} flex flex-col items-center justify-center space-y-4 p-6`}>
-         <h2 className="text-xl font-semibold">Downloading in Progress</h2>
-   
-         {/* Progress bar */}
-         <div className="relative w-full max-w-xs">
-           <div className="w-full h-2 bg-gray-200 rounded-full">
-             <div
-               className="h-2 bg-blue-500 rounded-full"
-               style={{ width: `${downloadPercentage}%` }}
-             ></div>
-           </div>
-   
-           {/* Percentage Text */}
-           <span
-             className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-6 text-xl font-medium text-blue-500"
-             style={{ left: `${downloadPercentage}%` }}
-           >
-             {downloadPercentage}%
-           </span>
-         </div>
-   
-         {/* Optional message */}
-         {downloadPercentage === 100 && (
-           <p className="text-green-500">Download Complete!</p>
-         )}
-       </div>
-   
-           </>
-   
-       )
-   }
-   
-   <div
-    onClick={()=>{
-       if(clickedList == 2){
-                 setClickedList(0);
-       }
-       else {
-           setClickedList(2);
-       }
-       
-      }}
-   className="w-full h-12 flex justify-between items-center p-2 bg-gradient-to-r from-green-500 to-green-300 hover:shadow-lg transition-shadow duration-300">
-     
-     <div className="w-1/2 h-full flex justify-center items-center overflow-x-hidden">
-       <span className="text-2xl text-white font-semibold w-full text-center overflow-x-hidden hover:text-gray-200 transition-colors duration-300">
-         name
-       </span>
-     </div>
-   
-    
-     <div className="w-1/2 h-full flex justify-center items-center">
-      
-       <div className="w-1/2 h-full flex justify-center items-center">
-         <span className="text-2xl text-blue-500 font-semibold hover:text-blue-400 transition-colors duration-300">
-           12
-           <span className="text-red-400 font-bold">:</span>
-           <span className="text-yellow-600">24</span>
-         </span>
-       </div>
-   
-       
-       <div className="w-1/2 h-full flex justify-center items-center">
-         <img
-           src="/download.png"
-           className="h-full w-auto hover:scale-110 transition-transform duration-300"
-           alt=""
-         />
-       </div>
-     </div>
-   </div>
-   {
-       clickedList == 2 && (
-           <div className='w-full p-4 bg-white h-44' >
-               <video controls muted autoPlay onError={handleError2} src={secondVideo} className={` ${fetchedErrorVideoShow2 ? "hidden":""} w-full h-full`} ></video>
-                <div className={` ${fetchedErrorVideoShow2 ? "":"hidden"} w-full h-full bg-gray-300 text-red-300 flex justify-center items-center`}>
-                             <span className='text-blue-300 font-semibold' >YOU NEED TO CONNECT YOUR DEVICE TO INTERNET</span>
-                </div>
-           </div>
-   
-       )
-   }
                           </div>
-                        )
-                       }
-                       {
-                        activeButton == 1 && (
-                          <DownloadedVideo/>
-                        )
-                       }
-         
-        </div>
+                 </div>
+                 <div 
+                 onClick={()=>{
+                     setActiveButton(0);
+                 }}
+                 className={`w-1/2 ${backColorGrade} flex relative z-20 justify-center items-center h-full  p-4 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300 group`}>
+<div className="text-white h-full text-2xl font-semibold group-hover:scale-105 transition-transform duration-300 ease-in-out">
+Grade
+</div>
+</div>
+                 <div
+                 onClick={()=>{
+                     setActiveButton(1);
+                 }}
+                 className={` ${backColorYVideos} w-1/2 flex relative z-20 justify-center items-center h-full  p-4 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300 group`}>
+<div className="text-white h-full text-2xl font-semibold group-hover:scale-105 transition-transform duration-300 ease-in-out">
+Your Videos
+</div>
+</div>
+                </div>
+                {
+                 activeButton == 0 && (
+                   <div className='mt-4 w-full h-auto py-4 bg-white flex flex-col gap-y-4 justify-start items-center' >
+                     {
+                        videoAsset.map((asset,index)=>(
+                          <React.Fragment key={asset.uniqueName}>
+                                   <div
+                                    
+                  
+                  className="w-full h-12 flex justify-between items-center p-2 bg-gradient-to-r from-green-500 to-green-300 hover:shadow-lg transition-shadow duration-300">
+
+<div 
+onClick={()=>{
+if(clickedList == index  && !downloadInProgressFirst){
+   setClickedList(0);
+ 
+}
+else{
+   setClickedList(index);
+}
+
+}}
+className="w-1/2 h-full flex justify-center items-center overflow-x-hidden">
+<span className="text-2xl text-white font-semibold w-full text-center overflow-x-hidden hover:text-gray-200 transition-colors duration-300">
+ {asset.uniqueName}
+</span>
+</div>
+
+
+<div className="w-1/2 h-full flex justify-center items-center">
+
+<div className="w-1/2 h-full flex justify-center items-center">
+ <span className="text-2xl text-blue-500 font-semibold hover:text-blue-400 transition-colors duration-300">
+   12
+   <span className="text-red-400 font-bold">:</span>
+   <span className="text-yellow-600">24</span>
+ </span>
+</div>
+
+
+<div className="w-1/2 h-full flex justify-center items-center">
+ <img
+ onClick={handleDownloadFirst}
+ id={index}
+   src="/download.png"
+   className={` ${handleDownloadFirstVideo == index ? "hidden":""} ${fullyDownloadedFirst == asset.uniqueName  ? "hidden":""} h-full w-auto hover:scale-110 transition-transform duration-300`}
+   alt=""
+ />
+  <img
+ onClick={()=>{
+    setPauseFirst(index);
+    setHandleDownloadFirstVideo(index);
+ }}
+   src="/pause.png"
+   className={` ${pauseFirst == index ? "":"hidden"} ${fullyDownloadedFirst == asset.uniqueName ? "hidden":""} h-full w-auto hover:scale-110 transition-transform duration-300`}
+   alt=""
+ />
+  <img
+ onClick={()=>{
+     console.log("finished clicked");
+ }}
+   src="/right.avif"
+   className={` ${fullyDownloadedFirst == asset.uniqueName ? "":"hidden"} h-full w-auto hover:scale-110 transition-transform duration-300`}
+   alt=""
+ />
+</div>
+</div>
+</div> 
+{
+clickedList == index && (
+  <>
+    <div
+     
+    className={` ${handleDownloadFirstVideo == index ? "":"hidden"} w-full p-4 bg-white h-44`} >
+         <video controls muted autoPlay onError={handleError} src={asset.src} className={` ${fetchedErrorVideoShow ? "hidden":""} w-full h-full`} ></video>
+         <div className={` ${fetchedErrorVideoShow ? "":"hidden"} w-full h-full bg-gray-300 text-red-300 flex justify-center items-center`}>
+                      <span className='text-blue-300 font-semibold' >YOU NEED TO CONNECT YOUR DEVICE TO INTERNET</span>
+         </div>
+    </div>
+     <div className={` ${handleDownloadFirstVideo == index ? "":"hidden"} flex flex-col items-center justify-center space-y-4 p-6`}>
+  <h2 className="text-xl font-semibold">Downloading in Progress</h2>
+
+  {/* Progress bar */}
+  <div className="relative w-full max-w-xs">
+    <div className="w-full h-2 bg-gray-200 rounded-full">
+      <div
+        className="h-2 bg-blue-500 rounded-full"
+        style={{ width: `${downloadPercentage}%` }}
+      ></div>
+    </div>
+
+    {/* Percentage Text */}
+    <span
+      className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-6 text-xl font-medium text-blue-500"
+      style={{ left: `${downloadPercentage}%` }}
+    >
+      {downloadPercentage}%
+    </span>
+  </div>
+
+  {/* Optional message */}
+  {downloadPercentage === 100 && (
+    <p className="text-green-500">Download Complete!</p>
+  )}
+</div>
+
+    </>
+
+)
+}
+<>
+
+</>
+                          </React.Fragment>
+                           
+                        ))
+                     }
+             
+ 
+
+ 
+ 
+                   </div>
+                 )
+                }
+                {
+                 activeButton == 1 && (
+                   <DownloadedVideo/>
+                 )
+                }
+  
+ </div>
+              )
+            }
+       
+        </>
     );
 };
 
