41dd80e765f003df2f120468230a0deb114a43cd
sabaif-s <sebaifmuhammed33@gmail.com>
2025-01-04 22:54:52 +0300

    fixing pdf reader canvas

--- src/components/pdf/PdfReader.jsx
+++ src/components/pdf/PdfReader.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
 import * as pdfjsLib from "pdfjs-dist";
+import ScreenSize from "../../hooks/ScreenSize";
 
 // Configure PDF.js worker
-
-
 
 const PDFViewer = ({ pdfBlob, handlingBack }) => {
   const containerRef = useRef(null);
@@ -12,6 +11,7 @@ const PDFViewer = ({ pdfBlob, handlingBack }) => {
   const [numPages, setNumPages] = useState(null);
   const [loading, setLoading] = useState(true);
   const [renderingPage, setRenderingPage] = useState(false);
+  const {isMobile}=ScreenSize();
 
   useEffect(() => {
     const loadPDF = async () => {
@@ -27,10 +27,13 @@ const PDFViewer = ({ pdfBlob, handlingBack }) => {
 
         // Load the PDF document
         const loadingTask = pdfjsLib.getDocument({
-          url: pdfUrl // 64 KB chunks
+          url: pdfUrl, // 64 KB chunks
         });
         const pdfDoc = await loadingTask.promise;
-        const pageFirst=await pdfDoc.getPage(1);
+        const pageFirst = await pdfDoc.getPage(1);
+        const viewport = pageFirst.getViewport({ scale: 1.0 });
+
+        console.log(`Width: ${viewport.width}, Height: ${viewport.height}`);
         console.log(pageFirst);
         setPdf(pdfDoc);
         setNumPages(pdfDoc.numPages);
@@ -62,9 +65,21 @@ const PDFViewer = ({ pdfBlob, handlingBack }) => {
       setRenderingPage(true);
 
       const page = await pdfDoc.getPage(pageNum);
-      const scale = calculateScale(); // Dynamic scaling based on container size
+      console.log("page dimension:", page.getViewport({ scale: 1 }));
+      const { width, height } = page.getViewport({ scale: 1 });
+      let ratio;
+      let high;
+      if (height > width) {
+        high = "height";
+        ratio = height / width;
+      } else {
+        high = "width";
+        ratio = width / height;
+      }
+      const scale = calculateScale(ratio, high); // Dynamic scaling based on container size
       const viewport = page.getViewport({ scale });
-
+      console.log("scale:", scale);
+      console.log("view port:", viewport);
       const canvas = document.createElement("canvas");
       containerRef.current.innerHTML = ""; // Clear the container before rendering
       containerRef.current.appendChild(canvas);
@@ -82,24 +97,41 @@ const PDFViewer = ({ pdfBlob, handlingBack }) => {
       await page.render(renderContext).promise;
     } catch (error) {
       console.error(`Error rendering page ${pageNum}:`, error);
+        
     } finally {
       setRenderingPage(false);
     }
   };
 
-  const calculateScale = () => {
-    if(containerRef.current != null){
-
- 
-    const containerWidth = containerRef.current.offsetWidth;
-    const containerHeight = containerRef.current.offsetHeight;
-    const pageAspectRatio = 1.5; // Adjust aspect ratio based on PDF content
-
-    return Math.min(
-      containerWidth / (pageAspectRatio * 600),
-      containerHeight / 900
-    );
-  }
+  const calculateScale = (ratio, high) => {
+    if (containerRef.current != null) {
+      const containerWidth = containerRef.current.offsetWidth;
+      const containerHeight = containerRef.current.offsetHeight;
+      console.log(containerHeight, containerWidth);
+      console.log(window.innerWidth);
+      const isMobile = window.innerWidth < 500 ? true : false;
+      console.log(isMobile);
+      const pageAspectRatio = ratio; // Adjust aspect ratio based on PDF content
+      if (high == "width") {
+        return Math.min(
+          containerWidth / (pageAspectRatio * 600),
+          containerHeight / 900
+        );
+      } else if (high == "height" && isMobile) {
+        return Math.min(1, containerWidth / (pageAspectRatio * 360), containerHeight / (pageAspectRatio * 300));
+      } else if (high == "height" && !isMobile) {
+        return Math.min(
+          1,
+          containerWidth / (pageAspectRatio * 300),
+          containerHeight / 600
+        );
+      } else {
+        return Math.min(
+          containerWidth / (pageAspectRatio * 900),
+          containerHeight / 600
+        );
+      }
+    }
   };
 
   const goToNextPage = async () => {
@@ -123,55 +155,56 @@ const PDFViewer = ({ pdfBlob, handlingBack }) => {
   };
 
   return (
-    <div
-    
-    className="relative z-30 w-full h-full bg-gray-50 flex flex-col items-center justify-center overflow-auto border border-gray-300 rounded-md shadow-lg p-4">
+    <div className={`relative z-30 w-full h-full bg-gray-50 flex flex-col items-center justify-center overflow-auto border border-gray-300 rounded-md shadow-lg ${isMobile ? "p-2":"p-4"} `}>
       {loading ? (
         <div className="flex justify-center items-center">
           <span className="text-xl text-gray-600">Loading PDF...</span>
         </div>
       ) : (
         <>
+        <div className={`w-full h-full ${isMobile ? "p-0":"p-4"} bg-red-100`} >
+
+        
           <div
             ref={containerRef}
             id="pdf-container"
-            className="relative w-full h-2/3 flex items-center justify-center bg-red-100 overflow-auto"
+            className="relative w-full h-full flex items-center justify-center overflow-auto"
             style={{ maxHeight: "100vh" }}
           ></div>
-         
+          </div>
         </>
       )}
-       <div className="flex flex-wrap gap-y-4 justify-between w-full mt-4">
-            <button
-              onClick={goToPreviousPage}
-              className="basis-1/3 text-white rounded-md"
-              disabled={pageNumber === 1 || renderingPage}
-            >
-              <span className="px-4 py-2 bg-blue-500 rounded-md text-white hover:bg-blue-600">
-                Previous
-              </span>
-            </button>
-            <span className="text-center basis-1/3 text-lg">
-              Page {pageNumber} of {numPages}
-            </span>
-            <button
-              onClick={goToNextPage}
-              className="basis-1/3 text-white rounded-md"
-              disabled={pageNumber === numPages || renderingPage}
-            >
-              <span className="px-4 py-2 bg-blue-500 rounded-md text-white hover:bg-blue-600">
-                Next
-              </span>
-            </button>
-            <button
-              onClick={backHandling}
-              className="basis-2/3 flex justify-end"
-            >
-              <span className="px-4 py-2 bg-blue-500 rounded-md text-white hover:bg-blue-600">
-                Back
-              </span>
-            </button>
-          </div>
+      <div className="flex flex-wrap gap-y-4 justify-between w-full mt-4">
+        <button
+          onClick={goToPreviousPage}
+          className="basis-1/3 text-white rounded-md"
+          disabled={pageNumber === 1 || renderingPage}
+        >
+          <span className="px-4 py-2 bg-blue-500 rounded-md text-white hover:bg-blue-600">
+            Previous
+          </span>
+        </button>
+        <span className="text-center basis-1/3 text-lg">
+          Page {pageNumber} of {numPages}
+        </span>
+        <button
+          onClick={goToNextPage}
+          className="basis-1/3 text-white rounded-md"
+          disabled={pageNumber === numPages || renderingPage}
+        >
+          <span className="px-4 py-2 bg-blue-500 rounded-md text-white hover:bg-blue-600">
+            Next
+          </span>
+        </button>
+        <button
+          onClick={backHandling}
+          className="basis-2/3 flex justify-end"
+        >
+          <span className="px-4 py-2 bg-blue-500 rounded-md text-white hover:bg-blue-600">
+            Back
+          </span>
+        </button>
+      </div>
     </div>
   );
 };
